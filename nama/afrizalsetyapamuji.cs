8b55d23dd6b4a807ac03676331cb3a7f8c615251
cuingskot76 <76592157+cuingskot76@users.noreply.github.com>
2021-10-05 22:25:54 +0700

    Afrizal_setya_pamuji.cs

---
+++ nama/afrizalsetyapamuji.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace latihan_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+
+            myName = "Afrizal Setya Pamuji";
+            
+            Console.WriteLine("My name is : " + myName);
+        }
+    }
+}
