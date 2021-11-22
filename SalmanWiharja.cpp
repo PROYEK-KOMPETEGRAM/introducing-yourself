2dce2e2bb13c9a301ad2d595fd7ed72e71fb52cf
Salman Abdul Jabbaar Wiharja <66382837+Salmansha08@users.noreply.github.com>
2021-11-22 13:00:53 +0700

    first commit
    nama lengkap

---
+++ SalmanWiharja.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string nama = "Salman Abdul Jabbaar Wiharja";
+    cout << "Nama Saya : " << nama << endl;
+  
+    return 0;
+}
